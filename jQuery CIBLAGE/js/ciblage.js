dddf9802a28edd2c144d9e4fb6083e5c45a38753
cpreudhomme64 <38043889+cpreudhomme64@users.noreply.github.com>
2018-06-19 15:39:36 +0200

    Découverte jQuery

---
+++ jQuery CIBLAGE/js/ciblage.js
@@ -0,0 +1,7 @@
+$('#selecteurJQ span:last').css('color', 'red');
+$('#selecteurJQ span:first').css('color', 'grey');
+
+
+var toto = document.querySelectorAll("span");
+toto[0].style.color = "red";
+toto[1].style.color = "grey";
