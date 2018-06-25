bc9d2f15a74dc0b8bdf997bdff18d06f4d4e6978
cpreudhomme64 <38043889+cpreudhomme64@users.noreply.github.com>
2018-06-25 13:43:55 +0200

    exercices PHP echo tableaux

---
+++ PHP/tableaux/tableaux_associatifs.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    
+    <?php
+
+$activites = array("Lundi"=>"Php","Mardi"=>"Javascript", "Mercredi"=>"Typescript","Jeudi"=>"Angular","Vendredi"=>"SQL","Samedi"=>"Python","Dimanche"=>"Tout");
+//deux dimensions
+$pierre = array("1"=>"Php","2"=>"Javascript","3"=>"Python");
+$paul = array("1"=>"Javascript","2"=>"Css","3"=>"Php");
+$jacques = array("1"=>"Java","2"=>"Php","3"=>"Javascript");
+$preferences = array();
+
+
+var_dump($activites);
+var_dump($pierre);
+
+// modify activity of sunday
+$activités = array_splice($activités, -1, 1, array("Dormir"));
+var_dump($activites);
+
+?>
+
+</body>
+</html>
