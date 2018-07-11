3fe1c4bd0e13c537509e459279df762f49728150
cpreudhomme64 <38043889+cpreudhomme64@users.noreply.github.com>
2018-07-11 22:55:19 +0200

    Add files via upload

---
+++ exo3 a 11/js/java.js
@@ -0,0 +1,175 @@
+// //EXO 3
+// var A;
+// var B;
+// var C;
+
+// A=3;
+// alert ("Ligne 1 : La valeur de A est " + A);
+// alert ("Ligne 1 : La valeur de B est " + B);
+// B=A+3;
+// alert ("Ligne 2 : La valeur de A est " + A);
+// alert ("Ligne 2 : La valeur de B est " + B);
+// A=4;
+// alert ("Ligne 3 : La valeur de A est " + A);
+// alert ("Ligne 3 : La valeur de B est " + B);
+
+//EXO4
+
+
+
+/*A=2.3;
+B=3.5;
+C=A+B;
+alert ("EXO4 La valeur de C est " + parseInt(C));
+
+//EXO5
+
+A=3;
+alert ("EXO5 Ligne 1 : La valeur de A est " + A);
+alert ("Ligne 1 : La valeur de B est " + B);
+B=5;
+alert ("Ligne 2 : La valeur de A est " + A);
+alert ("Ligne 2 : La valeur de B est " + B);
+C=A+B;
+alert ("Ligne 3 : La valeur de A est " + A);
+alert ("Ligne 3 : La valeur de B est " + B);
+alert ("Ligne 3 : La valeur de B est " + C);
+A=2;
+alert ("Ligne 4 : La valeur de A est " + A);
+alert ("Ligne 4 : La valeur de B est " + B);
+alert ("Ligne 4 : La valeur de B est " + C);
+C=B-A;
+alert ("Ligne 5 : La valeur de A est " + A);
+alert ("Ligne 5 : La valeur de B est " + B);
+alert ("EXO5 Ligne 5 : La valeur de C est " + C);*/
+
+
+//EXO6
+
+/*A=3;
+alert ("EXO6 Ligne 1 : La valeur de A est " + A);
+alert ("Ligne 1 : La valeur de B est " + B);
+alert ("Ligne 1 : La valeur de C est " + C);
+B=A+4;
+alert ("Ligne 2 : La valeur de A est " + A);
+alert ("Ligne 2 : La valeur de B est " + B);
+alert ("Ligne 1 : La valeur de C est " + C);
+A=A+2;
+alert ("Ligne 3 : La valeur de A est " + A);
+alert ("Ligne 3 : La valeur de B est " + B);
+alert ("Ligne 3 : La valeur de B est " + C);
+B=A-4;
+alert ("Ligne 4 : La valeur de A est " + A);
+alert ("Ligne 4 : La valeur de B est " + B);
+alert ("Ligne 4 : La valeur de Cest " + C);*/
+
+
+//EXO7
+
+/*A=5;
+alert ("EXO6 Ligne 1 : La valeur de A est " + A);
+alert ("Ligne 1 : La valeur de B est " + B);
+alert ("Ligne 1 : La valeur de C est " + C);
+B=10;
+alert ("Ligne 2 : La valeur de A est " + A);
+alert ("Ligne 2 : La valeur de B est " + B);
+alert ("Ligne 1 : La valeur de C est " + C);
+C=A+B;
+alert ("Ligne 3 : La valeur de A est " + A);
+alert ("Ligne 3 : La valeur de B est " + B);
+alert ("Ligne 3 : La valeur de B est " + C);
+B=A+B;
+alert ("Ligne 4 : La valeur de A est " + A);
+alert ("Ligne 4 : La valeur de B est " + B);
+alert ("Ligne 4 : La valeur de B est " + C);
+A=C;
+alert ("Ligne 5 : La valeur de A est " + A);
+alert ("Ligne 5 : La valeur de B est " + B);
+alert ("EXO5 Ligne 5 : La valeur de C est " + C);*/
+
+
+//EXO8
+
+/*var A;
+var B;
+var C;
+
+A=3;
+alert ("EXO6 Ligne 1 : La valeur de A est " + A);
+alert ("Ligne 1 : La valeur de B est " + B);
+
+B=5;
+alert ("Ligne 2 : La valeur de A est " + A);
+alert ("Ligne 2 : La valeur de B est " + B);
+
+C=A
+
+A=B;
+alert ("Ligne 3 : La valeur de A est " + A);
+alert ("Ligne 3 : La valeur de B est " + B);
+
+B=C;
+alert ("Ligne 4 : La valeur de A est " + A);
+alert ("Ligne 4 : La valeur de B est " + B);*/
+
+//EXO9
+
+/*var A = "123";
+var B = "456"; 
+alert (A+B)*/      
+
+//EXO11
+
+
+// var age = 16;
+
+
+
+// if (age<18) {
+//   alert("Vous avez moins de 18 ans");
+  
+// } else if (age<=19) {
+//   alert("Vous êtes un junior");
+
+// } else if (age<=22) {
+//   alert("Vous êtes un espoir");
+  
+// } else if (age<=39) {
+//   alert("Vous êtes un sénior");
+
+
+// } else {
+//   alert ("Vous êtes un vétéran");
+// }
+
+
+
+
+
+
+
+
+//var age = parseInt(prompt('Quel est votre âge?'));
+/*switch (age) {
+  case (19):
+    console.log("Vous êtes un espoir");
+    alert("Vous êtes un espoir");
+    break;
+  case (20<=age<=22):
+    console.log('Vous êtes un junior');
+    break;
+  case (23<=age<=39):
+    console.log('Vous êtes un sénior');
+    break;
+  case (age>=40):
+    console.log('Vous êtes un vétéran');
+    break;*/
+
+
+
+
+
+
+
+
+
